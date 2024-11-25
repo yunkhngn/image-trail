6af06c0040818eb6bbce615e853765b8c9899df9
yunkhngn <rogkhoa.mail@gmail.com>
2024-11-25 19:41:36 +0700

    Uploading img

--- src/components/ImageTrail.js
+++ src/components/ImageTrail.js
@@ -9,17 +9,15 @@ export const ImageTrail = () => {
       images={[
       "/image/1.jpg",
       "/image/2.PNG",
-        "/image/3.jpeg",
-        "/image/4.jpeg",
-        "/image/5.jpeg",
-        "/image/6.jpeg",
-        "/image/7.jpeg",
-        "/image/8.jpeg",
-        "/image/9.jpeg",
-        "/image/10.jpeg",
-        "/image/11.jpeg",
-        "/image/12.jpeg",
-
+      "/image/3.jpeg",
+      "/image/4.jpeg",
+      "/image/6.jpeg",
+      "/image/7.jpeg",
+      "/image/8.jpeg",
+      "/image/9.jpeg",
+      "/image/10.jpeg",
+      "/image/11.jpeg",
+      "/image/12.jpeg",
       ]}
     >
       <section className="img-trace">
