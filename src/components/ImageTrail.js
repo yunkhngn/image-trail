1cdf7701c26a425bd451a41d4ef794c116aa3deb
yunkhngn <rogkhoa.mail@gmail.com>
2024-11-25 19:32:30 +0700

    Updating favicon

---
+++ src/components/ImageTrail.js
@@ -0,0 +1,139 @@
+import { useAnimate } from "framer-motion";
+import React, { useRef } from "react";
+
+export const ImageTrail = () => {
+  return (
+    <MouseImageTrail
+      renderImageBuffer={50}
+      rotationRange={25}
+      images={[
+      "/image/1.jpg",
+      "/image/2.PNG",
+        "/image/3.jpg",
+        "/image/4.jpg",
+        "/image/5.jpg",
+        "/image/6.jpg",
+        "/image/7.jpg",
+        "/image/8.jpg",
+        "/image/9.jpg",
+        "/image/10.jpg",
+        "/image/11.jpg",
+        "/image/12.jpg",
+      ]}
+    >
+      <section className="img-trace">
+        <p className="container">
+          
+          <span>Hover me</span>
+        </p>
+      </section>
+    </MouseImageTrail>
+  );
+};
+
+const MouseImageTrail = ({
+  children,
+  // List of image sources
+  images,
+  // Will render a new image every X pixels between mouse moves
+  renderImageBuffer,
+  // images will be rotated at a random number between zero and rotationRange,
+  // alternating between a positive and negative rotation
+  rotationRange,
+}) => {
+  const [scope, animate] = useAnimate();
+
+  const lastRenderPosition = useRef({ x: 0, y: 0 });
+  const imageRenderCount = useRef(0);
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+
+    const distance = calculateDistance(
+      clientX,
+      clientY,
+      lastRenderPosition.current.x,
+      lastRenderPosition.current.y
+    );
+
+    if (distance >= renderImageBuffer) {
+      lastRenderPosition.current.x = clientX;
+      lastRenderPosition.current.y = clientY;
+
+      renderNextImage();
+    }
+  };
+
+  const calculateDistance = (x1, y1, x2, y2) => {
+    const deltaX = x2 - x1;
+    const deltaY = y2 - y1;
+
+    // Using the Pythagorean theorem to calculate the distance
+    const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+    return distance;
+  };
+
+  const renderNextImage = () => {
+    const imageIndex = imageRenderCount.current % images.length;
+    const selector = `[data-mouse-move-index="${imageIndex}"]`;
+
+    const el = document.querySelector(selector);
+
+    el.style.top = `${lastRenderPosition.current.y}px`;
+    el.style.left = `${lastRenderPosition.current.x}px`;
+    el.style.zIndex = imageRenderCount.current.toString();
+
+    const rotation = Math.random() * rotationRange;
+
+    animate(
+      selector,
+      {
+        opacity: [0, 1],
+        transform: [
+          `translate(-50%, -25%) scale(0.5) ${
+            imageIndex % 2
+              ? `rotate(${rotation}deg)`
+              : `rotate(-${rotation}deg)`
+          }`,
+          `translate(-50%, -50%) scale(1) ${
+            imageIndex % 2
+              ? `rotate(-${rotation}deg)`
+              : `rotate(${rotation}deg)`
+          }`,
+        ],
+      },
+      { type: "spring", damping: 15, stiffness: 200 }
+    );
+
+    animate(
+      selector,
+      {
+        opacity: [1, 0],
+      },
+      { ease: "linear", duration: 0.5, delay: 5 }
+    );
+
+    imageRenderCount.current = imageRenderCount.current + 1;
+  };
+
+  return (
+    <div
+      ref={scope}
+      className="wrapper"
+      onMouseMove={handleMouseMove}
+    >
+      {children}
+
+      {images.map((img, index) => (
+        <img
+          className="mouse-image"
+          src={img}
+          alt={`Mouse move ${index}`}
+          key={index}
+          data-mouse-move-index={index}
+        />
+      ))}
+    </div>
+  );
+};
